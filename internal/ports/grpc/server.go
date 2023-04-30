6779b29e59de28aeb2521f1014dce389d30ee282
mohammadne <mohammadne.dev@gmail.com>
2023-04-30 13:59:12 +0330

    feat: update server errors

--- internal/ports/grpc/server.go
+++ internal/ports/grpc/server.go
@@ -47,17 +47,23 @@ func (s *server) Serve(port int) {
 
 func (s *server) CreateTokenFromId(ctx context.Context, pbId *pb.Id) (*pb.Token, error) {
 	if pbId.Value == 0 {
-		return nil, errors.New("invalid")
+		errString := "Invalid id provider"
+		s.logger.Error(errString)
+		return nil, errors.New(errString)
 	}
 
 	payload, err := s.crypto.Encrypt(fmt.Sprint(pbId.Value))
 	if err != nil {
-
+		errString := "Error while encrypting the id"
+		s.logger.Error(errString, zap.Error(err))
+		return nil, errors.New(errString)
 	}
 
 	token, err := s.token.CreateTokenString(payload)
 	if err != nil {
-
+		errString := "Error while creating token from the payload"
+		s.logger.Error(errString, zap.Error(err))
+		return nil, errors.New(errString)
 	}
 
 	return &pb.Token{Value: token}, nil
@@ -66,17 +72,23 @@ func (s *server) CreateTokenFromId(ctx context.Context, pbId *pb.Id) (*pb.Token,
 func (s *server) GetIdFromToken(ctx context.Context, pbToken *pb.Token) (*pb.Id, error) {
 	var payload string
 	if err := s.token.ExtractTokenData(pbToken.Value, &payload); err != nil {
-
+		errString := "Error while extracting payload from token"
+		s.logger.Error(errString, zap.String("token", pbToken.Value), zap.Error(err))
+		return nil, errors.New(errString)
 	}
 
 	stringId, err := s.crypto.Decrypt(payload)
 	if err != nil {
-
+		errString := "Error while decrypting the payload"
+		s.logger.Error(errString, zap.String("payload", payload), zap.Error(err))
+		return nil, errors.New(errString)
 	}
 
 	id, err := strconv.ParseUint(stringId, 10, 64)
 	if err != nil {
-
+		errString := "Invalid id has been given"
+		s.logger.Error(errString, zap.String("id", stringId), zap.Error(err))
+		return nil, errors.New(errString)
 	}
 
 	return &pb.Id{Value: id}, nil
